c9f86acd654839d6d0cf44c7bad30aaabf6d182b
Arthur <embryarthur@gmail.com>
2023-12-05 15:36:07 -0600

    Refactor the indentation and formatting of the ProjectBody component.

--- src/components/projects/projectBody/main.jsx
+++ src/components/projects/projectBody/main.jsx
@@ -7,13 +7,13 @@ export default function ProjectBody({ title, date, description, company, childre
                 {company && <h3 className="font-semibold text-black">{company}</h3>}
                 <div className="flex justify-between items-center">
                     <h3 className="font-semibold">{title}</h3>
-                <span className="text-sm">{date}</span>
-            </div>
-            <p className="text-sm mb-4">{description}</p>
-            <div className="pl-5 space-y-2">
-                {children}
+                    <span className="text-sm">{date}</span>
+                </div>
+                <p className="text-sm mb-4">{description}</p>
+                <div className="pl-5 space-y-2">
+                    {children}
+                </div>
             </div>
         </div>
     );
-}
 }
