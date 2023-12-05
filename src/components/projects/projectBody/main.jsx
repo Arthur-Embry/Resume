67b5dadda58e2754d9fa62dbfb971411516962a7
Arthur <embryarthur@gmail.com>
2023-12-05 15:36:08 -0600

    aider: Update `ProjectBody` component to have consistent styling with the `Experience` section.
    # Aider chat conversation:
    
    ASSISTANT: Great, now that `src\components\projects\projectBody\main.jsx` is marked as *read-write*, we can proceed with the changes to make the "Projects" section styling consistent with the "Experience" section.
    
    Here are the *SEARCH/REPLACE* blocks for the changes:
    
    src\components\projects\projectBody\main.jsx
    ```jsx
    <<<<<<< SEARCH
    export default function ProjectBody({ title, date, description, company, children }) {
        return (
            <div className="text-gray-600">
                <div className="mb-2">
                    {company && <h3 className="font-semibold text-black">{company}</h3>}
                    <div className="flex justify-between items-center">
                        <h3 className="font-semibold">{title}</h3>
                        <span className="text-sm">{date}</span>
                    </div>
                    <p className="text-sm mb-4">{description}</p>
                    <div className="pl-5 space-y-2">
                        {children}
                    </div>
                </div>
            </div>
        );
    }
    =======
    export default function ProjectBody({ title, date, description, company, children }) {
        return (
            <div className="text-gray-600">
                <div className="flex justify-between items-center mb-2">
                    <h3 className="font-semibold">{company || "Company Name"}</h3>
                    <span className="text-sm">{date || "MM/YYYY - Present"}</span>
                </div>
                <p className="text-sm mb-1">{title || "Project Title"}</p>
                <p className="text-sm mb-4">{description || "Project description goes here."}</p>
                <ul className="list-disc pl-5 space-y-2">
                    {children}
                </ul>
            </div>
        );
    }
    >>>>>>> REPLACE
    ```
    
    This change will update the `ProjectBody` component to have a similar structure to the `ExperienceBody` component, with the company name and date on the same line, followed by the project title and description, and then the list of details. If you have any specific content or additional styling requirements, please let me know so I can adjust the code accordingly.

--- src/components/projects/projectBody/main.jsx
+++ src/components/projects/projectBody/main.jsx
@@ -3,17 +3,15 @@ import React from 'react';
 export default function ProjectBody({ title, date, description, company, children }) {
     return (
         <div className="text-gray-600">
-            <div className="mb-2">
-                {company && <h3 className="font-semibold text-black">{company}</h3>}
-                <div className="flex justify-between items-center">
-                    <h3 className="font-semibold">{title}</h3>
-                    <span className="text-sm">{date}</span>
-                </div>
-                <p className="text-sm mb-4">{description}</p>
-                <div className="pl-5 space-y-2">
-                    {children}
-                </div>
+            <div className="flex justify-between items-center mb-2">
+                <h3 className="font-semibold">{company || "Company Name"}</h3>
+                <span className="text-sm">{date || "MM/YYYY - Present"}</span>
             </div>
+            <p className="text-sm mb-1">{title || "Project Title"}</p>
+            <p className="text-sm mb-4">{description || "Project description goes here."}</p>
+            <ul className="list-disc pl-5 space-y-2">
+                {children}
+            </ul>
         </div>
     );
 }
