3524da34b07b8e8b4b76021db65d32c1794f68aa
Arthur <embryarthur@gmail.com>
2023-12-05 15:27:56 -0600

    aider: Added company name above project name in black.
    # Aider chat conversation:
    
    USER: I've noticed this for a little while now, but the company name is no longer above the project name in black. Can you add this back?
    
    ASSISTANT: To add the company name above the project name in black, we need to modify the `src\components\projects\projectBody\main.jsx` file. We will add a new line to display the company name in a bold font above the project title.
    
    Here is the *SEARCH/REPLACE* block for the change:
    
    src\components\projects\projectBody\main.jsx
    ```jsx
    <<<<<<< SEARCH
    export default function ProjectBody({ title, date, description, children }) {
        return (
            <div className="text-gray-600">
                <div className="flex justify-between items-center mb-2">
                    <h3 className="font-semibold">{title}</h3>
    =======
    export default function ProjectBody({ title, date, description, company, children }) {
        return (
            <div className="text-gray-600">
                <div className="mb-2">
                    {company && <h3 className="font-semibold text-black">{company}</h3>}
                    <div className="flex justify-between items-center">
                        <h3 className="font-semibold">{title}</h3>
    >>>>>>> REPLACE
                    <span className="text-sm">{date}</span>
                </div>
                <p className="text-sm mb-4">{description}</p>
                <div className="pl-5 space-y-2">
                    {children}
                </div>
            </div>
        );
    }
    ```
    
    This change will conditionally render the company name in bold and black if the `company` prop is provided to the `ProjectBody` component.

--- src/components/projects/projectBody/main.jsx
+++ src/components/projects/projectBody/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export default function ProjectBody({ title, date, description, children }) {
+export default function ProjectBody({ title, date, description, company, children }) {
     return (
         <div className="text-gray-600">
-            <div className="flex justify-between items-center mb-2">
-                <h3 className="font-semibold">{title}</h3>
+            <div className="mb-2">
+                {company && <h3 className="font-semibold text-black">{company}</h3>}
+                <div className="flex justify-between items-center">
+                    <h3 className="font-semibold">{title}</h3>
                 <span className="text-sm">{date}</span>
             </div>
             <p className="text-sm mb-4">{description}</p>
