198ac073f1dc3b439c98f645cb85c5f817e3cc55
Arthur <embryarthur@gmail.com>
2023-12-05 15:25:30 -0600

    aider: Indented all the content under the projects header in the `src\App.jsx` file.
    # Aider chat conversation:
    
    USER: I do like it split accross two lines, but I'd like everything under the projects header to be indented, not just "See https://www.syrahealth.com/behavioral-and-mental-health/syrenity for more details.
    
    As a result of this project, Syra Health was able to represent themselves as a tech-forward company. This enhancement raised the value of the company for an eventual IPO market cap of $26.01M."
    
    ASSISTANT: To indent all the content under the projects header, we need to modify the `src\App.jsx` file. We will adjust the indentation for the entire `ProjectBody` component instances within the `Projects` component.
    
    Here is the *SEARCH/REPLACE* block for the changes:
    
    src\App.jsx
    ```jsx
    <<<<<<< SEARCH
                {/* Syrenity Project at Syrahealth: */}
                <ProjectBody title="Syrenity - AI Telehealth Platform" date="Jan 2021 - Present" description="Spearheaded the Syrenity project to build AI-first telehealth platform as a progressive web app.">
                  <p className="text-sm mb-4">
                    See <a className="text-blue-300 hover:text-blue-200" href="https://www.syrahealth.com/behavioral-and-mental-health/syrenity" target="_blank">https://www.syrahealth.com/behavioral-and-mental-health/syrenity</a> for more details.
                  </p>
                  <p className="text-sm mb-4">
                    As a result of this project, Syra Health was able to represent themselves as a tech-forward company. This enhancement raised the value of the company for an eventual IPO market cap of $26.01M.
                  </p>
                </ProjectBody>
                {/* BlogMonster Project: */}
                <ProjectBody title="BlogMonster" date="TBD" description="A comprehensive blogging platform with advanced content management features, designed for OperatorAI." company="OperatorAI">
                  {/* Project details for BlogMonster can be added here as children */}
                </ProjectBody>
                {/* Add new projects below */}
    =======
                <div className="pl-5">
                  {/* Syrenity Project at Syrahealth: */}
                  <ProjectBody title="Syrenity - AI Telehealth Platform" date="Jan 2021 - Present" description="Spearheaded the Syrenity project to build AI-first telehealth platform as a progressive web app.">
                    <p className="text-sm mb-4">
                      See <a className="text-blue-300 hover:text-blue-200" href="https://www.syrahealth.com/behavioral-and-mental-health/syrenity" target="_blank">https://www.syrahealth.com/behavioral-and-mental-health/syrenity</a> for more details.
                    </p>
                    <p className="text-sm mb-4">
                      As a result of this project, Syra Health was able to represent themselves as a tech-forward company. This enhancement raised the value of the company for an eventual IPO market cap of $26.01M.
                    </p>
                  </ProjectBody>
                  {/* BlogMonster Project: */}
                  <ProjectBody title="BlogMonster" date="TBD" description="A comprehensive blogging platform with advanced content management features, designed for OperatorAI." company="OperatorAI">
                    {/* Project details for BlogMonster can be added here as children */}
                  </ProjectBody>
                  {/* Add new projects below */}
                </div>
    >>>>>>> REPLACE
    ```
    
    This change wraps all the `ProjectBody` components in a `div` with a left padding of `pl-5`, which will indent all the content under the projects header.

--- src/App.jsx
+++ src/App.jsx
@@ -60,20 +60,22 @@ export default function App() {
         {/* Column 2 */}
         <div className="w-2/5 p-2">
           <Projects>
-            {/* Syrenity Project at Syrahealth: */}
-            <ProjectBody title="Syrenity - AI Telehealth Platform" date="Jan 2021 - Present" description="Spearheaded the Syrenity project to build AI-first telehealth platform as a progressive web app.">
-              <p className="text-sm mb-4">
-                See <a className="text-blue-300 hover:text-blue-200" href="https://www.syrahealth.com/behavioral-and-mental-health/syrenity" target="_blank">https://www.syrahealth.com/behavioral-and-mental-health/syrenity</a> for more details.
-              </p>
-              <p className="text-sm mb-4">
-                As a result of this project, Syra Health was able to represent themselves as a tech-forward company. This enhancement raised the value of the company for an eventual IPO market cap of $26.01M.
-              </p>
-            </ProjectBody>
-            {/* BlogMonster Project: */}
-            <ProjectBody title="BlogMonster" date="TBD" description="A comprehensive blogging platform with advanced content management features, designed for OperatorAI." company="OperatorAI">
-              {/* Project details for BlogMonster can be added here as children */}
-            </ProjectBody>
-            {/* Add new projects below */}
+            <div className="pl-5">
+              {/* Syrenity Project at Syrahealth: */}
+              <ProjectBody title="Syrenity - AI Telehealth Platform" date="Jan 2021 - Present" description="Spearheaded the Syrenity project to build AI-first telehealth platform as a progressive web app.">
+                <p className="text-sm mb-4">
+                  See <a className="text-blue-300 hover:text-blue-200" href="https://www.syrahealth.com/behavioral-and-mental-health/syrenity" target="_blank">https://www.syrahealth.com/behavioral-and-mental-health/syrenity</a> for more details.
+                </p>
+                <p className="text-sm mb-4">
+                  As a result of this project, Syra Health was able to represent themselves as a tech-forward company. This enhancement raised the value of the company for an eventual IPO market cap of $26.01M.
+                </p>
+              </ProjectBody>
+              {/* BlogMonster Project: */}
+              <ProjectBody title="BlogMonster" date="TBD" description="A comprehensive blogging platform with advanced content management features, designed for OperatorAI." company="OperatorAI">
+                {/* Project details for BlogMonster can be added here as children */}
+              </ProjectBody>
+              {/* Add new projects below */}
+            </div>
           </Projects>
           <Skills />
           <ProgrammingLanguages />
