f565dcd0119c209ad85ab2c4f062da8ddd4a4875
Arthur <embryarthur@gmail.com>
2023-12-05 15:39:05 -0600

    aider: Reverted "Projects" section header style back to its original state.
    # Aider chat conversation:
    
    USER: revert projects back to normal, and let's see the full code of the projects section
    
    ASSISTANT: To revert the "Projects" section header style back to its original state, we need to modify the `src\components\projects\main.jsx` file. Here is the *SEARCH/REPLACE* block for the change:
    
    src\components\projects\main.jsx
    ```jsx
    <<<<<<< SEARCH
                <div className="max-w-2xl mx-auto p-6">
                    <h2 className="text-xl font-semibold mb-4">PROJECTS</h2>
                </div>
    =======
                <div className="border-b-2 border-black pb-2">
                    <h2 className="text-2xl font-semibold">PROJECTS</h2>
                </div>
    >>>>>>> REPLACE
    ```
    
    Now, to show you the full code of the "Projects" section after the change, here it is:
    
    src\components\projects\main.jsx
    ```jsx
    export default function Projects({ children }) {
        return (
            <div className="p-2">
                <div className="border-b-2 border-black pb-2">
                    <h2 className="text-2xl font-semibold">PROJECTS</h2>
                </div>
                {children}
            </div>
        );
    }
    ```
    This is the code after reverting the header style to its original state.

--- src/components/projects/main.jsx
+++ src/components/projects/main.jsx
@@ -2,8 +2,8 @@ export default function Projects({ children }) {
     return (
         <div className="p-2">
             <div className="max-w-2xl mx-auto my-8">
-                <div className="max-w-2xl mx-auto p-6">
-                    <h2 className="text-xl font-semibold mb-4">PROJECTS</h2>
+                <div className="border-b-2 border-black pb-2">
+                    <h2 className="text-2xl font-semibold">PROJECTS</h2>
                 </div>
                 {children}
             </div>
