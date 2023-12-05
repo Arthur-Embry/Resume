0ec454aadee87b8a8ab38c084b31ac9d9ca6c70b
Arthur <embryarthur@gmail.com>
2023-12-05 15:21:20 -0600

    Added main.jsx component for projects.

---
+++ src/components/projects/main.jsx
@@ -0,0 +1,27 @@
+export default function Projects() {
+    return (
+        <div className="p-2">
+            <div className="max-w-2xl mx-auto my-8">
+                <div className="border-b-2 border-black pb-2">
+                    <h2 className="text-2xl font-semibold">PROJECTS</h2>
+                </div>
+                <div className="max-w-2xl mx-auto p-6">
+                    <h2 className="text-xl font-semibold mb-4">Syrenity</h2>
+                    <div className="text-gray-600">
+                        <div className="flex justify-between items-center mb-2">
+                            <h3 className="font-semibold">Syra Health</h3>
+                            <span className="text-sm">01/2023 - Present</span>
+                        </div>
+                        <p className="text-sm mb-1">Carmel, Indiana, United States - Remote</p>
+                        <p className="text-sm mb-4">
+                            Spearheaded the Syrenity project to build AI first telehealth platform as a progressive web app. See <a className="text-blue-300 hover:text-blue-200" href="https://www.syrahealth.com/behavioral-and-mental-health/syrenity" target="_blank">https://www.syrahealth.com/behavioral-and-mental-health/syrenity</a> for more details.
+                        </p>
+                        <p className="text-sm mb-4">
+                            As a result of this project, Syra Health was able to represent themselves as tech forward company. This enhancement raised the value of the company for an eventual IPO market cap of $26.01M.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
